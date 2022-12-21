8fcf6d45eb8177d73e7f2a8592b0cf785bd6414c
Charlesg154 <Charlesgreenjr@comcast.net>
2022-12-21 17:26:16 +0000

    Playing with what I\'ve learned so far

---
+++ name_day.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def main():
+
+    name = input("\nPardon me, but what is your name?  ")
+
+    print("\nI see. ", name, end="") 
+
+    day = input(", might you tell me what today is?  ")
+
+    print("\nHello,", name+"!  Happy", day + "!")
+
+main()
