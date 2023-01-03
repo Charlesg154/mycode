d377d08a6e215eff7ae7d63c5814b501b1ca5237
Charlesg154 <Charlesgreenjr@comcast.net>
2023-01-03 22:25:19 +0000

    Updating my name day function

--- name_day.py
+++ name_day.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-
+"""
+An input asking the user's name.
+An input asking what day of the week it is.
+A print statement that reads:
+Hello, <name>! Happy <day of the week>!
+"""
 def main():
 
     name = input("\nPardon me, but what is your name?  ")
@@ -8,6 +13,6 @@ def main():
 
     day = input(", might you tell me what today is?  ")
 
-    print("\nHello,", name+"!  Happy", day + "!\n")
+    print("\nHello,", name + "!  Happy", day + "!\n")
 
 main()
