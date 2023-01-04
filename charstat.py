a6b5dd4844a7e208297516799824c2399f671130
Charlesg154 <Charlesgreenjr@comcast.net>
2023-01-04 22:02:04 +0000

    learning about dictionaries

--- charstat.py
+++ charstat.py
@@ -19,12 +19,18 @@ marvelchars= {
 
 def main():
     name=input(f"Please enter the character's name. Choose between {marvelchars.keys()}\n>")
-    name=name.lower();name=name.capitalize()
+    name=name.title()
     print(f"You chose {name}")
     stat=input(f"\nPlease enter a statistic between {marvelchars['Starlord'].keys()}\n>")
     stat=stat.lower()
     print(f"You chose {stat}")
-    print(f"\n{name}'s {stat} is: {marvelchars[name][stat]}")
+    print(f"\n{name}'s {stat} is: {marvelchars[name][stat].title()}")
+    repeat=input("\nWould you like to play again?  y/n\n>")
+    repeat=repeat.lower()
+    if repeat == "y":
+        main()
+    else:
+        print("Goodbye")
 
 
 
