6a05d43af6245a2622276746a0e947636b1c7a57
Charlesg154 <Charlesgreenjr@comcast.net>
2023-01-04 21:32:28 +0000

    learning about dictionaries

---
+++ charstat.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+marvelchars= {
+"Starlord":
+  {"real name": "peter quill",
+  "powers": "dance moves",
+  "archenemy": "Thanos"},
+
+"Mystique":
+  {"real name": "raven darkholme",
+  "powers": "shape shifter",
+  "archenemy": "Professor X"},
+
+"Hulk":
+  {"real name": "bruce banner",
+  "powers": "super strength",
+  "archenemy": "adrenaline"}
+             }
+
+def main():
+    name=input(f"Please enter the character's name. Choose between {marvelchars.keys()}\n>")
+    name=name.lower();name=name.capitalize()
+    print(f"You chose {name}")
+    stat=input(f"\nPlease enter a statistic between {marvelchars['Starlord'].keys()}\n>")
+    stat=stat.lower()
+    print(f"You chose {stat}")
+    print(f"\n{name}'s {stat} is: {marvelchars[name][stat]}")
+
+
+
+main()
