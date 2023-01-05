156e5e2b65cef24bff244b268419c6ebf445236a
Charlesg154 <Charlesgreenjr@comcast.net>
2023-01-05 17:25:29 +0000

    Rock paper sci

--- rock-paper-sci.py
+++ rock-paper-sci.py
@@ -9,24 +9,28 @@ import random
 def main():
     """body of the game"""
 
-    point=0
-    cpoint=0
+    point=0 # Variable to keep track of user point
+    cpoint=0 # Variable to keep track of CPU point
 
-    while point != 4:
-        choice= input("\nRock, Paper, or Scissors?\n>")
-        botchoice= random.choice(["rock", "paper", "scissors"])
+    while point != 4: # At the end of the game I'm going to set the point value to 4 to stop the game.
+
+        choice= input("Rock, Paper, or Scissors?\n>")#Have user input their choice
+        botchoice= random.choice(["rock", "paper", "scissors"])#Have CPU make their choice
 
         choice= choice.lower() # validates input by forcing input to be lower case
 
-      # uncomment these print functions to debug
+        #print choices
         print(f"You chose {choice}!\n")
         print(f"CPU chose {botchoice}!\n")
 
-        if choice not in ["rock", "paper", "scissors"]:
+        if choice not in ["rock", "paper", "scissors"]:#If the user enters an invalid option, give CPU a point
             print("You entered an invalid choice, you lose(r)!")
             cpoint+=1
+
         elif choice == botchoice:#Draw, no point
             print("Draw")
+
+        #Define win conditions.  Give user a point
         elif choice == "scissors" and botchoice == "paper":
             print("You win!")
             point+=1
@@ -36,23 +40,23 @@ def main():
         elif choice == "rock" and botchoice == "scissors":
             print("You win!")
             point+=1
+        
+        #Anything else would be a loss.  Give cpu a point
         else:
             print("You lose!")
             cpoint+=1
-        print(f"Score is {point}/{cpoint}")
+        
+        #Print current score
+        print(f"Score is {point}/{cpoint}\n")
+
+        #See if either user or CPU has meant winning score of two.
+        #If so, print win or lose message and set point value to 4 to end the loop
         if point == 2:
             print("Congratulations!  You win the game!")
             point=4
         elif cpoint == 2:
             print("CPU wins the game!")
             point=4
-
-
-
-
-    # user picked scissors... did they win or lose?
-
-    ### ADD MORE HERE
 
 
 main()
