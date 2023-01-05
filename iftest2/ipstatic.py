de0c7b583d97329b4b5d13eff17c8786b40453c8
Charlesg154 <Charlesgreenjr@comcast.net>
2023-01-05 16:01:36 +0000

    quiz work

---
+++ iftest2/ipstatic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+ipchk = input("Apply an IP address: ")
+
+#a string tests as True
+if ipchk == "192.168.70.1":
+    print(f"Looks like the IP address of the Gateway was set: {ipchk}  This is not recommended.")
+elif ipchk:
+    print("Looks like the IP address was set: " + ipchk)
+else:
+    print("You did not provide input")
---
+++ what-kind-of-pokemon-are-you.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+#Dictionary for Pokémon
+quiz={"Question 1":
+    {"Which color is your favorite?": "Question",
+        "Red": "Charmander",
+        "Blue": "Squirtle",
+        "Green": "Bulbasaur",
+        "Yellow": "Pikachu"},
+    "Question 2":
+    {"What is your favorite climate?": "Question",
+        "Doesn’t matter as long as I can stretch and walk on my own!": "Pikachu",
+        "Surrounded by Nature!  I wanna feel the grass between my toes.": "Bulbasaur",
+        "Anywhere with water leaves me feeling nice and refreshed.": "Squirtle",
+        "Hot and dry!  Keep that rain away from me!": "Charmander"},
+    "Question 3":
+    {"How do you do the things you do?": "Question",
+        "The only way that number one should!": "Bulbasaur",
+        "I like to be slick!": "Squirtle",
+        "Make one wrong mvoe and I will kick your grass": "Charmander",
+        "Pika?": "Pikachu"}
+    }
+
+bulba=""
+squira=""
+charma=""
+pikaa=""
+
+def main():
+    x=input("What variable would you like to check?\n>")
+    x=x.upper()
+    if(x=="B"):
+        print(bulba)
+    elif(x=="S"):
+        print(squira)
+    elif(x=="C"):
+        print(charma)
+    elif(x=="P"):
+        print(pikaa)
+    elif(x=="Q"):
+        print(quiz)
+    else:
+        print(quiz["Question 1"])
+        print("Goodbye")
+
+main()
