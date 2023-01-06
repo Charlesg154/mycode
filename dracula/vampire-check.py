039ee15ef66a5d73c3ec1e1d8f9295675745e08d
Charlesg154 <Charlesgreenjr@comcast.net>
2023-01-06 23:40:18 +0000

    Vampire stuff!

--- dracula/vampire-check.py
+++ dracula/vampire-check.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 """
-Find Vampire 
+Find Lines w/ Vampire in Dracula!
 """
 def main():
-
+    v=0
+    with open('vampytimes.txt',"w") as vampy:
+            vampy.write(" ")
+    with open('dracula.txt',"r") as book:
+        for line in book:
+            if "vampire" in line.lower():
+                print(line)
+                v+=1
+                with open('vampytimes.txt',"a") as vampy:
+                    vampy.write(line)
+    print(f"Vampire is written on {v} lines in Dracula!")
 main()
 
